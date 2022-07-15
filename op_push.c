7d0e5ec2f378b3932014f9164169b56c56179fbe
Al-hussary1 <Al-hussary@outlook.com>
2022-07-15 00:14:23 -0700

    push_opcode

--- op_push.c
+++ op_push.c
@@ -1,32 +1,53 @@
 #include "monty.h"
-
 /**
- * push - push an element to the stack
- * @head: head pointer(at bottom of stack)
- * @line_number: bytecode line number
+ * op_push - pushes a value to the top of the stack
+ * @head: A pointer to head node
+ * @line_number: line number where opcode was encountered
  */
-void push(stack_t **head, unsigned int line_number)
+void op_push(stack_t **head, unsigned int line_number)
 {
-	stack_t *new;
+	stack_t *new_node = NULL, *tail = NULL;
+	size_t idx = 0;
 
-	if (head == NULL)
-	{
-		printf("L%u: usage: push integer\n", line_number);
+	if (!head || !line_number)
 		exit(EXIT_FAILURE);
-	}
-
-	new = malloc(sizeof(stack_t));
-	if (new == NULL)
+	if (stack_val.n[idx] == '\0')
+		push_error(head, line_number);
+	new_node = malloc(sizeof(stack_t));
+	if (!new_node)
+		malloc_error(head);
+/* make sure string contains only numbers and '-' */
+	while (stack_val.n[idx] != '\0')
 	{
-		printf("Error: malloc failed\n");
-		free_stack(head, line_number);
-		exit(EXIT_FAILURE);
+		if (isdigit(stack_val.n[idx]) == 0 && stack_val.n[idx] != '-')
+			push_error(head, line_number);
+		idx++;
 	}
-	new->n = variable.value;
-	new->prev = NULL;
-	new->next = *head;
+	new_node->n = atoi(stack_val.n);
 
-	if (*head != NULL)
-		(*head)->prev = new;
-	*head = new;
+	if (stack_val.qu == 1)
+	{
+		new_node->next = NULL;
+		if (!*head)
+		{
+			new_node->prev = NULL;
+			*head = new_node;
+		}
+		else
+		{
+			tail = *head;
+			while (tail->next)
+				tail = tail->next;
+			new_node->prev = tail;
+			tail->next = new_node;
+		}
+	}
+	else
+	{
+		new_node->prev = NULL;
+		new_node->next = *head;
+		if (*head)
+			(*head)->prev = new_node;
+		*head = new_node;
+	}
 }
